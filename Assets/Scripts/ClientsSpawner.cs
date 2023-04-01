12016ed86300ad3e3830099fc689bc1f9c7bb7a5
mirotvoretts <panoffilya@gmail.com>
2023-04-02 00:19:32 +0500

    Clients Spawner

---
+++ Assets/Scripts/ClientsSpawner.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ClientsSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _client;
+
+    private Timer _timer;
+
+    private void Awake()
+    {
+        _timer = new Timer();
+    }
+
+    private void Update()
+    {
+        _timer.Set(Config.ClientsSpawnDelay, SpawnClient);
+    }
+
+    private void SpawnClient()
+    {
+        Instantiate(_client, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/System/Config.cs
@@ -0,0 +1,4 @@
+﻿public static class Config
+{
+    public const float ClientsSpawnDelay = 35f;
+}
